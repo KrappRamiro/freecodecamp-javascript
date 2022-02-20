6a155bbb5c84d38ca69dfed639613cb102f23226
Ramiro Krapp <krappramiro@disroot.org>
2022-02-20 09:26:46 -0300

    added the save tab button

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,9 +1,9 @@
 let myLeads = [];
-
-let inputEl = document.getElementById("input-el");
-let saveInputBtn = document.getElementById("save-input-btn");
-let deleteBtn = document.getElementById("delete-btn");
+const inputEl = document.getElementById("input-el");
+const saveInputBtn = document.getElementById("save-input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
+const saveTabBtn = document.getElementById("save-tab-btn");
 
 /* -------------- START OF LOADING PREVIOUSLY STORED DATA -------------- */
 //get leads from localStorage and stores it in myLeads
@@ -14,32 +14,22 @@ console.log("leads from local storage: ", leadsFromLocalStorage);
 if (leadsFromLocalStorage) {
 	//if its not null
 	myLeads = leadsFromLocalStorage;
-	renderLeads();
+	renderArray(myLeads, myLeads);
 }
-
 /* -------------- END OF LOADING PREVIOUSLY STORED DATA ---------------- */
 
-function renderLeads() {
+function renderArray(array, html_var_to_load) {
 	//log out the variable
-
-	//one way, inneficient because manipulating the DOM comes with a cost
-	if (false) {
-		ulEl.textContent = "";
-		for (let i = 0; i < myLeads.length; i++) {
-			// innerHTML works like innerText, but it allows to put html
-			ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-		}
-	}
 
 	//another way, more efficient because it manipulates the DOM once:
 	// it saves all the pages in listItems, and finally it assigns the ulEl innerHTML to be equal to that
 	let listItems = "";
-	for (let i = 0; i < myLeads.length; i++) {
+	for (let i = 0; i < array.length; i++) {
 		// this is a template string, it allows easier HTML formatting in my js code
 		listItems += `
 		<li>
-			<a target='_blank' href='${myLeads[i]}'>
-				${myLeads[i]}
+			<a target='_blank' href='${html_var_to_load[i]}'>
+				${array[i]}
 			</a>
 		</li>
 		`;
@@ -55,12 +45,25 @@ saveInputBtn.addEventListener("click", function () {
 	// saving myLeads[] to local storage
 	localStorage.setItem("myLeads", JSON.stringify(myLeads));
 
-	renderLeads(); //renders the stored leads
+	renderArray(myLeads, myLeads); //renders the stored leads
 });
 
 deleteBtn.addEventListener("dblclick", function () {
 	console.log("Deleting the stored leads");
 	myLeads = [];
 	localStorage.clear();
-	renderLeads();
+	renderArray(myLeads, myLeads);
+});
+
+saveTabBtn.addEventListener("click", function () {
+	// save the url of the current tab
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		myLeads.push(tabs[0].url); //add the tab to the myLeads array
+		localStorage.setItem("myLeads", JSON.stringify(myLeads));
+		renderArray(myLeads, myLeads);
+	});
+
+	// saving myLeads[] to local storage
+
+	renderArray(myLeads, myLeads); //renders the stored leads
 });
